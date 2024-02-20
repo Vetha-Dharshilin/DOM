3c450e36408d23a0cdc34def122b3cdd78b0f6a1
Vetha-Dharshilin <merlinshylaja25@gamil.com>
2024-02-20 12:11:24 +0530

    uploading file

---
+++ script.js
@@ -0,0 +1,7 @@
+function foo(){
+    var ele1 = document.getElementById("firstname").value;
+    var ele2 = document.getElementById("middlename").value;
+    var ele3 = document.getElementById("lastname").value;
+    var ele=document.getElementById("email").value;
+    console.log(ele,ele1,ele2,ele3);
+}
